a3fd91bfda4f7a4d123f38da50e05b827795cd22
hannaLu7 <annlut52@gmail.com>
2024-12-27 18:42:00 -0600

    Update HelloCydeo.java

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -5,9 +5,11 @@ public class HelloCydeo {
     public static void main(String[] args) {
 
         System.out.println("Hello Cydeo");
+        //some change are added.
+
     }
 
-    //some change are added.
+
 
     }
 
